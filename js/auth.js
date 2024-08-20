683370de35ef94478e80db52d99b5fe3156a50e3
Frank <franco@craftech.io>
2024-08-19 22:50:50 -0300

    updatenew

--- js/auth.js
+++ js/auth.js
@@ -1,36 +1,37 @@
 var CLIENT_ID = "654333069607-8bb0ect8b87n5pdjkp1ptb463r15pb09.apps.googleusercontent.com"
-/** 
-document.addEventListener('DOMContentLoaded', function() {
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  var id_token = googleUser.getAuthResponse().id_token;
+
+  // Guardar el token en el almacenamiento local si es necesario
+  sessionStorage.setItem('id_token', id_token);
+
+  // Mostrar el contenido de la página si está autenticado
+  document.getElementById('auth-controls').style.display = 'none';
+  document.getElementById('content').style.display = 'block';
+
+  // Cargar la vista principal
+  loadView('apps', 'Apps');
+
+  // Actualizar la interfaz con la información del usuario
+  document.getElementById('user-name').textContent = profile.getName();
+  document.getElementById('user-image').src = profile.getImageUrl();
+}
+
+function initGoogleAuth() {
   gapi.load('auth2', function() {
       gapi.auth2.init({
           client_id: CLIENT_ID
-      }).then(function(auth2) {
-          if (auth2.isSignedIn.get()) {
-              updateUIWithUser(auth2.currentUser.get().getBasicProfile());
-          } else {
-              showHelpOnly();
+      }).then(function () {
+          const authInstance = gapi.auth2.getAuthInstance();
+          if (authInstance.isSignedIn.get()) {
+              onSignIn(authInstance.currentUser.get());
           }
       });
   });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  initGoogleAuth();
 });
-
-function updateUIWithUser(profile) {
-  // Mostrar los elementos que dependen de la sesión activa
-  document.querySelectorAll('.menu-item').forEach(el => el.style.display = 'block');
-  document.getElementById('user-info').style.display = 'flex';
-
-  // Actualizar la información del usuario
-  document.getElementById('user-name').textContent = profile.getName();
-  document.getElementById('user-image').src = profile.getImageUrl();
-
-  // Cargar la vista de bienvenida por defecto
-  loadWelcomeView();
-}
-
-function showHelpOnly() {
-  // Solo el botón de ayuda se mostrará
-  document.querySelectorAll('.menu-item').forEach(el => el.style.display = 'none');
-  document.getElementById('help').style.display = 'block';
-  document.getElementById('user-info').style.display = 'none';
-}
-*/ 
