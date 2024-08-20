d0b1f2ca39533abaa819815dd045436f64910a58
Frank <franco@craftech.io>
2024-08-20 19:39:51 -0300

    updatenew

--- js/auth.js
+++ js/auth.js
@@ -22,7 +22,7 @@ function handleCredentialResponse_old(response) {
   document.getElementById('user-image').src = profile.picture;
 }
 
-function parseJwt2(token) {
+function parseJwt(token) {
   const base64Url = token.split('.')[1];
   const base64 = decodeURIComponent(atob(base64Url).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
--- views/signin/signin.js
+++ views/signin/signin.js
@@ -1,12 +1,18 @@
 var CLIENT_ID = "654333069607-t118hpn2v2ui383h9fcfpo0aspiv4tva.apps.googleusercontent.com"
 
 function initializeView() {
-    console.log("executed")
+
     google.accounts.id.initialize({
         client_id: CLIENT_ID,
         callback: handleCredentialResponse
-      });
-      google.accounts.id.prompt();
+    });
+
+
+    google.accounts.id.renderButton(
+        document.getElementById("g_id_onload"),
+        { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt();
 
 }
 
@@ -14,27 +20,26 @@ function handleCredentialResponse(response) {
     console.log("LOGGEO!!!!")
     const credential = response.credential;
     const profile = parseJwt(credential);
-  
+
     // Guardar el token en el almacenamiento local si es necesario
     sessionStorage.setItem('id_token', credential);
-  
+
     // Mostrar el contenido de la página si está autenticado
-    document.getElementById('auth-controls').style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-  
+    document.getElementById('headerItem').style.display = 'block';
+
     // Cargar la vista principal
     loadView('apps', 'Apps');
-  
+
     console.log(profile)
     // Actualizar la interfaz con la información del usuario
     document.getElementById('user-name').textContent = profile.name;
     document.getElementById('user-image').src = profile.picture;
-  }
-  
-  function parseJwt2(token) {
+}
+
+function parseJwt(token) {
     const base64Url = token.split('.')[1];
-    const base64 = decodeURIComponent(atob(base64Url).split('').map(function(c) {
+    const base64 = decodeURIComponent(atob(base64Url).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
     return JSON.parse(base64);
-  }
+}
