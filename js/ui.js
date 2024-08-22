11ab15bdb04c6b5d541ed41ed6fa342be73ac76b
Frank <franco@craftech.io>
2024-08-21 23:40:04 -0300

    updatenew

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,6 @@
 var CLIENT_ID = "654333069607-t118hpn2v2ui383h9fcfpo0aspiv4tva.apps.googleusercontent.com"
 
-console.log("VER:" + "1.3")
+console.log("VER:" + "1.4")
 
 document.addEventListener('DOMContentLoaded', function() {
     const currentHash = window.location.hash.substring(1); // Elimina el '#' del hash
